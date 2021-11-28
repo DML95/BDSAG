b3b22e75255a7675cf533ce370fd68b726bae251
David <montilla.david95@gmail.com>
2021-11-28 17:01:39 +0100

    correccion linux

--- src/core/restFunctions/restFunctionCommun.cpp
+++ src/core/restFunctions/restFunctionCommun.cpp
@@ -5,3 +5,11 @@ void RESTFunctionCommun::createMessageError(rapidjson::Document &document,const 
     document.SetObject();
     document.AddMember("error",error,allocator);
 }
+
+bool RESTFunctionCommun::filter(Constant::methods method,const std::vector<Constant::nodes> &nodes) {
+	return false;
+}
+
+int RESTFunctionCommun::operation(Response &response,const Request &request) {
+	return 404;
+}
